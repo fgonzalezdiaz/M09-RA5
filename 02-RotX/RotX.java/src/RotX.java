3b04e8edf46dcf95d2f10652c173468f28bf3cee
fgonzalezdiaz <fgonzalezd24042005@gmail.com>
2025-10-01 22:14:47 +0200

    Exercici ROTX ACABAT

--- 02-RotX/RotX.java/src/RotX.java
+++ 02-RotX/RotX.java/src/RotX.java
@@ -1,33 +1,47 @@
 public class RotX {
     private static char[] abecedariMinuscules = "aàábcçdeèéfghiïíjklmnñoòópqrstuüúvwxyz".toCharArray();
     private static char[] abecedariMajuscules = "AÀÁBCÇDEÈÉFGHIÏÍJKLMNÑOÒÓPQRSTUÜÚVWXYZ".toCharArray();
+
     public static void main(String[] args){
-        String[] llistaText = new String[]{"ABC", "XYZ", "Hola, Mr. calçot", "Perdó, per tu què és"};
+        String[] llistaText = new String[]{"ABC", "XYZ", "Hola, Mr. calçot", "Perdó, per tu què és?"};
         String[] llistaXifrada = new String[llistaText.length];
+        int desplaçament = 50;
+        int desplaçamentInicial = desplaçament;
         System.out.println("Xifrat");
         System.out.println("------");
         for(int i = 0; i < llistaText.length ; i++){
-            System.out.println(xifraRotX(llistaText[i], 4));
-            llistaXifrada[i] += xifraRotX(llistaText[i], 4);
+            String xifrat = xifraRotX(llistaText[i], desplaçament);
+            System.out.printf("(%d)   %-30s => %s%n", desplaçament, llistaText[i], xifrat);
+            llistaXifrada[i] = xifrat;
+            desplaçament += 2;
         }
-        
-        System.out.println("---------");
+        System.out.println();
         System.out.println("Desxifrat");
         System.out.println("---------");
-        for(int i = 0; i < llistaText.length ; i++){
-            System.out.println(desxifraRotX(llistaText[i], 4));
+        desplaçament = desplaçamentInicial;
+        
+        for(int i = 0; i < llistaXifrada.length ; i++){
+            String desxifrat = desxifraRotX(llistaXifrada[i], desplaçament);
+            System.out.printf("(%d)   %-30s => %s%n", desplaçament, llistaXifrada[i], desxifrat);
+            desplaçament += 2;
+        }
+
+        System.out.println();
+        System.out.println("Missatge xifrat => " + llistaXifrada[llistaXifrada.length - 1]);
+        System.out.println("---------------------------------");
+        String[] llistaDesxifrada = forcaBrutaRotX( llistaXifrada[llistaXifrada.length - 1]);
+        for(int i = 0; i < llistaDesxifrada.length; i++){
+            System.out.println(llistaDesxifrada[i]);
         }
     }
+
     public static String xifraRotX(String cadena, int desplacament){
         return RotXUno(cadena, desplacament, true);
     }
+
     public static String desxifraRotX(String cadena, int desplacament){
         return RotXUno(cadena, desplacament, false);
     }
-
-    /*public static String forcaBrutaRotX(String cadenaXifrada){
-        for(int i)
-    }*/
 
     public static String RotXUno(String cadena, int desplacament, boolean xifrar){
         String textARetornar = "";
@@ -41,8 +55,15 @@ public class RotX {
                 textARetornar += c;
             }
         }
-
         return textARetornar;
+    }
+
+    public static String[] forcaBrutaRotX(String cadenaXifrada){ 
+        String[] textosDesxifrats = new String[abecedariMajuscules.length];
+        for(int i = 0 ; i < abecedariMajuscules.length; i++){
+            textosDesxifrats[i] = "("+ i + ") " + desxifraRotX(cadenaXifrada, i);
+        }
+        return textosDesxifrats;
     }
 
     public static int cercaIndex(char c, boolean xifrar, int desplacament){
@@ -56,18 +77,14 @@ public class RotX {
             }
         }
 
-        if(xifrar == true){
-            numero += desplacament;
-            while (numero > abecedariMajuscules.length){
-                numero %= 38;
-            }
-            return numero;
+        if(xifrar){
+            numero = (numero + desplacament) % abecedariMajuscules.length;
         } else {
-            numero -= desplacament;
-            while (numero < 0){
-                numero += 38;
+            numero = (numero - desplacament) % abecedariMajuscules.length;
+            if (numero < 0) {
+                numero += abecedariMajuscules.length;
             }
-            return numero;
         }
+        return numero;
     }
 }
